0490fd695596d6bdeb68684a092d7f73c1090ad4
Brunna Mattos <rjbrunna@outlook.com>
2020-09-02 15:13:11 -0300

    CSS adicionado. Lista editável.

--- js/index.js
+++ js/index.js
@@ -3,8 +3,9 @@ var list = [];
 
 const addNew = () => {
     setTableDiv();
+    setTableBody();
     setList();
-}
+};
 
 //create 'table' body
 const setTableDiv = () => {
@@ -12,19 +13,53 @@ const setTableDiv = () => {
     const tableElement = getTableDivElement();
     titleFormElement.innerHTML = `<h1>Your to do list!</h1>`;
     tableElement.innerHTML = `
-        <table>
-            <tr>
-                <th>Nº</th>
-                <th>To do</th>
-            </tr>
+        <table class="table table-striped table-borderless">
+            <thead id="tableHead" class="thead-dark">
+                <tr>
+                    <th>#</th>
+                    <th>To do</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody id="tableBody">
+                
+            </tbody>
         </table>
     `
-}
+};
+
+const setTableBody = () => {
+    let tableTask = "";
+    for (let i = 0; i<list.length; i++) {
+        console.log(list[i]);
+        tableTask += `
+            <tr>
+                <td class="table-name">${i+1}</td>
+                <td class="table-url">${list[i]}</td>
+                <td><a class="edit-icon" onclick="editItem(${i});">&#9999;</a></td>
+                <td><a class="remove-icon" onclick="removeItem(${i});">&#128465;</a></td>
+            </tr>
+        `
+    }
+
+    tableBody.innerHTML = tableTask;
+};
+
 
 const setList = () => {
     var message = getToDoMessage();
     list.push(message);
+};
+
+const editItem = (i) => {
+    list[i] = "eu fui alterado";
+    setTableBody();
 }
+
+// const removeItem = (i) => {
+    
+// }
 
 //Query Selectors
 const getTableDivElement = () => document.querySelector("#table-list");
