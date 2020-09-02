b0480ca7c763b6112bb9e662630c5b93281c2d0c
Brunna Mattos <rjbrunna@outlook.com>
2020-09-02 16:55:16 -0300

    Função de excluir adicionada. Design otimizado. Interação com o usuário melhorada.

--- js/index.js
+++ js/index.js
@@ -30,16 +30,31 @@ const setTableDiv = () => {
 
 const setTableBody = () => {
     let tableTask = "";
-    for (let i = 0; i<list.length; i++) {
-        console.log(list[i]);
+    if (list[0] == null) {
         tableTask += `
-            <tr>
-                <td class="table-name">${i+1}</td>
-                <td class="table-url">${list[i]}</td>
-                <td><a class="edit-icon" onclick="editItem(${i});">&#9999;</a></td>
-                <td><a class="remove-icon" onclick="removeItem(${i});">&#128465;</a></td>
-            </tr>
+        <tr>
+            <td class="table-index"></td>
+            <td class="table-message">
+                <p>&#10004; You have nothing planned for today!<br>
+                <br>&#9728; Soak up a sun for a tan &#127958;<br>
+                &#10052; Or take advantage of Alaskan snow to make a snowman &#9731;<br></p>
+            </td>
+            <td></td>
+            <td></td>
+        </tr>
         `
+    }
+    else {
+        for (let i = 0; i<list.length; i++) {
+            tableTask += `
+                <tr>
+                    <td class="table-index">${i+1}</td>
+                    <td class="table-message">${list[i]}</td>
+                    <td><a class="edit-icon" onclick="editItem(${i});">&#9999;</a></td>
+                    <td><a class="remove-icon" onclick="removeItem(${i});">&#128465;</a></td>
+                </tr>
+            `
+        }
     }
 
     tableBody.innerHTML = tableTask;
@@ -52,16 +67,19 @@ const setList = () => {
 };
 
 const editItem = (i) => {
-    list[i] = "eu fui alterado";
+    list[i] = "I was changed";
     setTableBody();
 }
 
-// const removeItem = (i) => {
-    
-// }
+const removeItem = (i) => {
+    list.splice(i,1);
+    setTableBody();
+}
+
 
 //Query Selectors
 const getTableDivElement = () => document.querySelector("#table-list");
 const getTitleFormDivElement = () => document.querySelector("#title-form");
 const getToDoInput = () => document.querySelector("#toDo-message");
 const getToDoMessage = () => document.querySelector("#toDo-message").value;
+const getEditModal = () => document.querySelector("#textEditModal");
